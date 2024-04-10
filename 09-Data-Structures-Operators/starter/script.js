c78c20a9904e78a2c48d5c9c800152e401e8682e
ostandrey <ost.and.g@gmail.com>
2024-04-10 11:23:30 +0300

    challenge #3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1014,3 +1014,38 @@ const firstBookMap = new Map(Object.entries(books[0]));
 for (const [key, value] of firstBookMap) {
   // if (typeof value === 'number') console.log(key);
 }
+
+// =====> Challenge 3
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '� Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '� Substitution'],
+  [64, '� Yellow card'],
+  [69, '� Red card'],
+  [70, '� Substitution'],
+  [72, '� Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '� Yellow card'],
+]);
+
+// 1.
+const events = [...new Set(gameEvents.values())];
+
+// 2.
+gameEvents.delete(64);
+
+// 3.
+const time1 = [...gameEvents.keys()].pop();
+// console.log(
+//   `An event happened, on average, every ${time1 / gameEvents.size} minutes`
+// );
+
+// console.log(time1);
+
+// 4.
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  // console.log(`[${half} Half] ${min}: ${event}`);
+}
